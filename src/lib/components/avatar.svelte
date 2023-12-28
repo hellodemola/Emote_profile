c379991f828264dc2711eaaa153968f2117a372a
Ademola Onasoga <ademola.onasoga@gmail.com>
2023-12-28 13:12:00 +0100

    add play icon to avatar

---
+++ src/lib/components/avatar.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let hasIntroVideo = true
+</script>
+
+<div id="avatar" class="relative">
+    <div class="w-[6em]">
+    <img
+    alt="user avatar"
+    src="profile.png"
+    width="100%"
+    />
+    </div>
+    {#if hasIntroVideo }
+    <div class="absolute bottom-0 right-0 ">
+    <img alt="play video"
+    src="play_icon.svg"
+    width="100%"
+    />
+    </div>
+    {/if}
+</div>
