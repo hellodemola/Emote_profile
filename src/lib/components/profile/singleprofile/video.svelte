907abb2db981a0c8bf84e68d02eed3d017d64d63
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-01-02 19:50:19 +0100

    add img

--- src/lib/components/profile/singleprofile/video.svelte
+++ src/lib/components/profile/singleprofile/video.svelte
@@ -1,4 +1,4 @@
- <div class="my-[2em] rounded-[16px]  h-fit shadow-md shadow-[0_40px_40px_0px_rgba(0, 0, 0, 0.05)] bg-[url(bg_image.svg)] bg-cover bg-no-repeat flex justify-center items-center cursor-pointer">
+ <div class="my-[2em] rounded-[16px]  h-fit shadow-md shadow-[0_40px_40px_0px_rgba(0, 0, 0, 0.05)] bg-[url(bg_img.jpeg)] bg-cover bg-center bg-no-repeat flex justify-center items-center cursor-pointer">
                 <div class="h-[588px] position flex items-center justify-center">
                     <img alt="play" class="" src="play.svg" />
                 </div>
