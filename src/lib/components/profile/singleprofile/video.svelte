3e3ec1577342bc283ea3e0b5c390a6b7b2247ff7
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-01-02 19:35:05 +0100

    fix image

--- src/lib/components/profile/singleprofile/video.svelte
+++ src/lib/components/profile/singleprofile/video.svelte
@@ -1,5 +1,5 @@
- <div class="my-[2em] p-[18px] py-[36px] rounded-[16px] border border-[#F1F1F1] bg-white h-fit shadow-md shadow-[0_40px_40px_0px_rgba(0, 0, 0, 0.05)] h-[588px] bg-[url(bg_image.svg)] bg-cover bg-no-repeat flex justify-center items-center">
-                <div>
-                    <img alt="play" src="play.svg" />
+ <div class="my-[2em] rounded-[16px]  h-fit shadow-md shadow-[0_40px_40px_0px_rgba(0, 0, 0, 0.05)] bg-[url(bg_image.svg)] bg-cover bg-no-repeat flex justify-center items-center cursor-pointer">
+                <div class="h-[588px] position flex items-center justify-center">
+                    <img alt="play" class="" src="play.svg" />
                 </div>
             </div>
